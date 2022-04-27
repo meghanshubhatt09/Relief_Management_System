5aaa37be0efac82f625884bf9c17c4578447b45b
ArjunNandkishorTiwari <tiwari.ar@northeastern.edu>
2022-04-27 15:10:07 -0400

    generating request for blood
    Signed-off-by: ArjunNandkishorTiwari <tiwari.ar@northeastern.edu>

--- Relief Management System/src/ui/Community/RequestBloodFromBankJPanel.java
+++ Relief Management System/src/ui/Community/RequestBloodFromBankJPanel.java
@@ -8,7 +8,10 @@ import Business.BloodBank.BloodBank;
 import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.UserAccount.UserAccount;
+import Business.WorkQueue.CommunityBloodRequest;
+import Business.WorkQueue.CommunityDonationRequest;
 import java.awt.CardLayout;
+import java.util.Date;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JPanel;
 
@@ -53,6 +56,9 @@ public class RequestBloodFromBankJPanel extends javax.swing.JPanel {
         txtPacketCount = new javax.swing.JTextField();
         txtVenue = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        btnSubmitRequest = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        txtPurpose = new javax.swing.JTextField();
 
         tblRequestTableCommunity.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -95,6 +101,21 @@ public class RequestBloodFromBankJPanel extends javax.swing.JPanel {
             }
         });
 
+        btnSubmitRequest.setText("Submit Request");
+        btnSubmitRequest.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSubmitRequestActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setText("jLabel5");
+
+        txtPurpose.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtPurposeActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -108,18 +129,23 @@ public class RequestBloodFromBankJPanel extends javax.swing.JPanel {
                         .addGap(57, 57, 57)
                         .addComponent(jButton1)
                         .addGap(173, 173, 173)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnSubmitRequest)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(214, 214, 214)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(jLabel5))
                         .addGap(147, 147, 147)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(txtPacketCount, javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(bloodGroupComboBox, javax.swing.GroupLayout.Alignment.TRAILING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(txtVenue))))
+                            .addComponent(txtVenue)
+                            .addComponent(txtPurpose))))
                 .addContainerGap(64, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -142,7 +168,13 @@ public class RequestBloodFromBankJPanel extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtVenue, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 233, Short.MAX_VALUE)
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(txtPurpose, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                .addComponent(btnSubmitRequest)
+                .addGap(133, 133, 133)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -159,6 +191,29 @@ public class RequestBloodFromBankJPanel extends javax.swing.JPanel {
         layout.previous(userProcessContainer);
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void btnSubmitRequestActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitRequestActionPerformed
+        // TODO add your handling code here:
+        
+        CommunityBloodRequest request = new CommunityBloodRequest();
+        
+        request.setRequestedBloodType(bloodGroupComboBox.getSelectedItem().toString());
+        request.setNoBloodPacketsRequired(Integer.parseInt(txtPacketCount.getText()));
+        request.setLocation(txtVenue.getText());
+        Date date = new Date();
+        request.setRequestDate(date);
+        request.setStatus("Requested");
+        request.setSender(account);
+        request.setPurpose(txtPurpose.getText());
+        account.getWorkQueue().getWorkRequestList().add(request);
+        enterprise.getWorkQueue().getWorkRequestList().add(request);
+        ecoSystem.getWorkQueue().getWorkRequestList().add(request);
+        
+    }//GEN-LAST:event_btnSubmitRequestActionPerformed
+
+    private void txtPurposeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPurposeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtPurposeActionPerformed
+
     
      private void populateBloodGroupComboBox(){
         DefaultComboBoxModel modelDoctorType = new DefaultComboBoxModel();
@@ -173,14 +228,17 @@ public class RequestBloodFromBankJPanel extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> bloodGroupComboBox;
+    private javax.swing.JButton btnSubmitRequest;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblRequestTableCommunity;
     private javax.swing.JTextField txtPacketCount;
+    private javax.swing.JTextField txtPurpose;
     private javax.swing.JTextField txtVenue;
     // End of variables declaration//GEN-END:variables
 }
